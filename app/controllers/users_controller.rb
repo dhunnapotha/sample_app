fe2b2556c2fc9d736053134c9f2b1fe93b460014
gc <gautam4you@gmail.com>
2013-09-04 20:01:06 +0530

    Signup, Edit, Delete, Modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,26 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
-		@user.save!
+		if @user.save!
+			flash[:success] = "Successfully added the user"
+			redirect_to @user
+		else
+			flash[:failure] = "Something is screwed. Please check the errors !"
+			render 'new'
+		end
+		
+			# flash a message ? 
+			# and then redirect to where ?
+	  		#flash[:success] = "Welcome to the Sample App!"
+	   	# else
+		# 	render 'new'
+		# end
+
 	end
 
-
+	def index
+		@users = User.all
+	end
 	def dummy
 		User.create(name: 'gautam', email: 'gautam1@moontara.com')
 	end
@@ -18,11 +34,43 @@ class UsersController < ApplicationController
 	end
 
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			redirect_to @user 
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		if @user.destroy
+			flash[:success] = "Successfully deleted the user"
+			respond_to do |format|
+				format.html { redirect_to :action => "index" }
+				format.js
+			end
+		else
+			flash[:failure] = "Failed to delete the user"
+			respond_to do |format|
+				format.html { redirect_to :action => "index" }
+				format.js
+			end
+		end
+
+	end
+
 	private
+
 	def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-  	end
+		params.require(:user).permit(:name, :email, :password,
+			:password_confirmation)
+	end
 
 
 end
